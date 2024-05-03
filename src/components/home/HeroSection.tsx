8c4090774008eb9a9d8a2e39dad718f74a1980a6
13prashant <pacific1392@gmail.com>
2024-05-04 00:09:46 +0530

    created hero-section

--- src/components/home/HeroSection.tsx
+++ src/components/home/HeroSection.tsx
@@ -1,3 +1,33 @@
+import Image from "next/image";
+import { Button } from "../ui/button";
+
 export default function HeroSection() {
-  return <section>HeroSection</section>;
+  return (
+    <section className="cr-container flex flex-col-reverse gap-10 md:flex-row duration-300">
+      <div className="flex flex-col gap-7 text-center md:text-left max-w-3xl duration-300">
+        <h2 className="text-4xl md:text-5xl font-extrabold cr-text-gradient duration-300">
+          Impulsione suas vendas de forma automática e com muita escala com
+          ChatResponde.
+        </h2>
+        <p className="text-xl md:text-2xl duration-300">
+          Potencialize seus resultados com nosso chatbot, permitindo interações
+          ilimitadas com clientes simultaneamente. Automatize suas vendas,
+          economize tempo e amplie sua presença digital.
+        </p>
+        <Button className="font-bold md:w-fit px-14 cr-bg-gradient hover:cr-bg-reverse-gradient">
+          Quero fazer parte!
+        </Button>
+      </div>
+
+      {/* This margin is for compensating image's white space */}
+      <div className="md:-mt-14">
+        <Image
+          src="/assets/home/mobile-chat.png"
+          alt="Mobile screen showing chat"
+          width={1400}
+          height={1400}
+        />
+      </div>
+    </section>
+  );
 }
