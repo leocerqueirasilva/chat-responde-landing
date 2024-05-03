0f20de439e5502ec66641458b143c59be0dca129
13prashant <pacific1392@gmail.com>
2024-05-04 01:57:16 +0530

    created our-resources section

--- src/components/home/OurResources.tsx
+++ src/components/home/OurResources.tsx
@@ -1,3 +1,36 @@
+import Image from "next/image";
+import { resources } from "./helpers";
+
 export default function OurResources() {
-  return <section>OurResources</section>;
+  return (
+    <section className="cr-container">
+      <div className="flex flex-col gap-4 text-center">
+        <h3 className="text-4xl font-extrabold">Descubra nossos recursos</h3>
+        <p className="text-lg md:text-xl duration-300">
+          Explore as vantagens que oferecemos para impulsionar seu negócio com
+          eficiência e facilidade.
+        </p>
+      </div>
+
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-x-10 gap-y-16 mt-20 duration-300">
+        {resources.map((resource) => (
+          <div
+            key={resource.id}
+            className="relative bg-muted border-2 border-secondary rounded-xl p-10 text-center"
+          >
+            <p className="text-lg font-extrabold">{resource.title}</p>
+            <p className="mt-5">{resource.description}</p>
+
+            <Image
+              className="absolute right-1/2 translate-x-1/2 -top-8"
+              src={resource.icon}
+              alt={resource.title}
+              width={60}
+              height={60}
+            />
+          </div>
+        ))}
+      </div>
+    </section>
+  );
 }
---
+++ src/components/home/helpers/index.ts
@@ -0,0 +1,43 @@
+export const resources = [
+  {
+    id: 0,
+    title: "Eficiência",
+    description:
+      "Nossos clientes relatam o aumento exponencial de suas vendas com o uso de nosso sistema",
+    icon: "/assets/home/our-resources/efficiency.svg",
+  },
+  {
+    id: 1,
+    title: "Praticidade",
+    description: "Rápido de configurar e já começar a vender",
+    icon: "/assets/home/our-resources/practicality.svg",
+  },
+  {
+    id: 2,
+    title: "Simplicidade",
+    description:
+      "Pensado de forma inteligente para quem não é profissional com tecnologia",
+    icon: "/assets/home/our-resources/simplicity.svg",
+  },
+  {
+    id: 3,
+    title: "Automatização 24h por dia",
+    description:
+      "O chatbot funciona de forma ininterrupta e permite você focar nas atividades que precisa fazer",
+    icon: "/assets/home/our-resources/automation.svg",
+  },
+  {
+    id: 4,
+    title: "Custo benefício",
+    description:
+      "A plataforma que oferece muito mais cobrando muito menos, para que você possa ter resultados hoje mesmo",
+    icon: "/assets/home/our-resources/cost-benefit.svg",
+  },
+  {
+    id: 5,
+    title: "Conexão",
+    description:
+      "A inteligência artificial reconhece aquilo que o seu cliente precisa rapidamente, e realizar a venda",
+    icon: "/assets/home/our-resources/connection.svg",
+  },
+];
