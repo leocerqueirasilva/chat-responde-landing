6709ca935401fef1a8516f6368d288a0f6da453e
13prashant <pacific1392@gmail.com>
2024-05-04 00:18:13 +0530

    created custom-flow section

--- src/components/home/CustomFlows.tsx
+++ src/components/home/CustomFlows.tsx
@@ -1,3 +1,16 @@
+import Image from "next/image";
+
 export default function CustomFlows() {
-  return <section>CustomFlows</section>;
+  return (
+    <section className="cr-container">
+      <div className="flex justify-center">
+        <Image
+          src="/assets/home/custom-flow.png"
+          alt="UI showing building custom flow"
+          width={1000}
+          height={1000}
+        />
+      </div>
+    </section>
+  );
 }
