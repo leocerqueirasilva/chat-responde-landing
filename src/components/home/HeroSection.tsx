5cb960c44b1ccbef6e65f1d4bd618bc9d3b03085
13prashant <pacific1392@gmail.com>
2024-05-04 20:52:04 +0530

    improved mobile-chat image

--- src/components/home/HeroSection.tsx
+++ src/components/home/HeroSection.tsx
@@ -3,7 +3,7 @@ import { Button } from "../ui/button";
 
 export default function HeroSection() {
   return (
-    <section className="cr-container flex flex-col-reverse gap-10 md:flex-row duration-300">
+    <section className="cr-container flex flex-col-reverse gap-10 md:flex-row items-center duration-300">
       <div className="flex flex-col gap-7 text-center md:text-left max-w-3xl duration-300">
         <h2 className="text-4xl md:text-5xl font-extrabold cr-text-gradient duration-300">
           Impulsione suas vendas de forma automática e com muita escala com
@@ -17,15 +17,12 @@ export default function HeroSection() {
         <Button className="font-bold md:w-fit">Quero fazer parte!</Button>
       </div>
 
-      {/* This margin is for compensating image's white space */}
-      <div className="md:-mt-14">
-        <Image
-          src="/assets/home/mobile-chat.png"
-          alt="Mobile screen showing chat"
-          width={1400}
-          height={1400}
-        />
-      </div>
+      <Image
+        src="/assets/home/mobile-chat.png"
+        alt="Mobile screen showing chat"
+        width={450}
+        height={450}
+      />
     </section>
   );
 }
