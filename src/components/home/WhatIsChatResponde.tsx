870fdd1e36234edbbb0e09d731edd09beb0880cb
13prashant <pacific1392@gmail.com>
2024-05-04 21:01:21 +0530

    added play button

--- src/components/home/WhatIsChatResponde.tsx
+++ src/components/home/WhatIsChatResponde.tsx
@@ -4,7 +4,7 @@ export default function WhatIsChatResponde() {
   return (
     <section>
       <div className="cr-bg-gradient">
-        <div className="cr-container !pb-60">
+        <div className="cr-container !pt-36 !pb-60">
           <div className="flex flex-col gap-4 text-center max-w-4xl mx-auto">
             <h3 className="text-4xl font-extrabold">O que é o ChatResponde</h3>
             <p className="text-lg md:text-xl duration-300">
@@ -16,13 +16,22 @@ export default function WhatIsChatResponde() {
         </div>
       </div>
 
-      <Image
-        className="rounded-tl-md blur-sm mx-auto -mt-52"
-        src="/assets/home/high-scale.png"
-        alt="UI showing building custom flow"
-        width={550}
-        height={550}
-      />
+      <div className="relative -mt-52">
+        <Image
+          className="rounded-tl-md blur-sm mx-auto"
+          src="/assets/home/high-scale.png"
+          alt="UI showing building custom flow"
+          width={550}
+          height={550}
+        />
+        <Image
+          className="absolute top-1/2 -translate-y-1/2 left-1/2 -translate-x-1/2 cursor-pointer hover:scale-95 duration-300"
+          src="/assets/icons/play.svg"
+          alt="Play button"
+          width={50}
+          height={50}
+        />
+      </div>
     </section>
   );
 }
