dc90075de81f37c2354b19d733d5a51440aa9e14
13prashant <pacific1392@gmail.com>
2024-05-04 13:06:49 +0530

    added logo to signup-form

--- src/components/common/Logo.tsx
+++ src/components/common/Logo.tsx
@@ -1,15 +1,26 @@
 import Image from "next/image";
+import { cn } from "@/lib/utils";
 
-export default function Logo() {
+type LogoProps = {
+  className?: string;
+  size?: "lg";
+};
+export default function Logo({ className, size }: LogoProps) {
   return (
-    <div className="flex gap-2 items-center">
+    <div className={cn("flex gap-2 items-center", className)}>
       <Image
         src="/assets/logo.svg"
         alt="ChatResponde logo"
-        width={30}
-        height={30}
+        width={size === "lg" ? 50 : 30}
+        height={size === "lg" ? 50 : 30}
       />
-      <span className="font-extrabold text-xl">ChatResponde</span>
+      <span
+        className={cn("font-extrabold text-xl", {
+          "text-4xl": size === "lg",
+        })}
+      >
+        ChatResponde
+      </span>
     </div>
   );
 }
--- src/components/home/SignUpForm.tsx
+++ src/components/home/SignUpForm.tsx
@@ -1,9 +1,14 @@
 import { Input } from "../ui/input";
 import { Button } from "../ui/button";
+import Logo from "../common/Logo";
 
 export default function SignUpForm() {
   return (
-    <section className="cr-container">
+    <section className="cr-container !pt-0">
+      <div className="flex justify-center mb-10 md:mb-20 duration-300">
+        <Logo className="md:hidden" />
+        <Logo className="hidden md:flex" size="lg" />
+      </div>
       <div className="cr-bg-gradient rounded-2xl text-center px-10 py-16 lg:px-20 lg:py-16 flex flex-col items-center justify-between gap-4 lg:flex-row duration-300">
         <h3 className="text-xl font-extrabold">
           Fique por dentro das nossas novidades!
